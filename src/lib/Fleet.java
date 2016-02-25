84bb7eedfb00e30c39a7f2997634bec79e6ae1cd
Dharmendra Singh <dharmenn@thoughtworks.com>
2016-02-25 20:00:29 +0530

    Resolved conflicts with remote

--- src/lib/Fleet.java
+++ src/lib/Fleet.java
@@ -1,13 +1,33 @@
 package lib;
 
 public class Fleet {
-    protected Ship[] fleet;
+    private Ship[] fleet = new Ship[5];
+    private String[] ships = {"BATTLESHIP", "CARRIER", "CRUISER", "DESTROYER", "SUBMARINE"};
+    private int[] size = {4, 5, 3, 2, 3};
+
     public Fleet() {
-        Ship battleship = new Ship("Battleship", 4);
-        Ship carrier = new Ship("Carrier", 5);
-        Ship cruiser = new Ship("Cruiser", 3 );
-        Ship destroyer = new Ship("Destroyer", 2 );
-        Ship submarine = new Ship("Submarine", 3 );
-        this.fleet = new Ship[]{battleship, carrier, cruiser,destroyer,submarine};
+        for (int i = 0; i < ships.length; i++) {
+            this.fleet[i] = new Ship( ships[i], size[i] );
+        }
+    }
+
+    public int size() {
+        return fleet.length;
+    }
+
+    public boolean deployeShipAt(String shipName, String position, String orientation) throws Exception {
+        Ship ship = getShip( shipName );
+        int size = ship.getSize();
+        /*Here grid class will be used to validate the ship deployment*/
+        String [] positions = {"A1","B1","C1","D1"};
+        ship.deployAtPositions(positions);
+        return true;
+    }
+
+    private Ship getShip(String name){
+        for (Ship ship : fleet)
+            if (ship.getName().equals( name.toUpperCase( ) ))
+                return ship;
+        throw  new RuntimeException( "Ship "+name+" does not exists");
     }
 }
--- test/FleetTest.java
+++ test/FleetTest.java
@@ -1,6 +1,7 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import lib.Fleet;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
